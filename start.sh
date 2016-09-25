edc4fa9cb810b6ad06bc9b4ff556bca609f34f39
Mohsen Kashi <m.kashi@rayanhamafza.com>
2016-09-25 16:11:24 +0330

    add Timing

--- start.sh
+++ start.sh
@@ -8,12 +8,15 @@ sbt compile
 cd ..
 echo "compilation finished. Now benchmarking starts:"
 cd java
-java JavaTest
+echo "-----------------------------------------------------JAVA-----------------------------------------------------"
+time java JavaTest
 cd ..
 cd scala
-sbt run
+echo "-----------------------------------------------------SCALA-----------------------------------------------------"
+time sbt run
 cd ..
 cd swfexec
-.build/debug/swfexec
+echo "-----------------------------------------------------SWIFT-----------------------------------------------------"
+time .build/debug/swfexec
 cd ..
 echo "END!!"
